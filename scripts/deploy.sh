b003057e7be95fdd620e9810bc25d004bca7e304
sujinlee <soongjing@sujinlees-MacBook-Pro.local>
2025-03-28 11:48:50 +0900

    frontend ci/cd

---
+++ scripts/deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+REPOSITORY=/home/ec2-user/project
+cd $REPOSITORY
+
+echo "> 현재 실행 중인 프로세스 확인"
+CURRENT_PID=$(pgrep -f "python3 -m http.server 8080")
+
+if [ -z "$CURRENT_PID" ]; then
+  echo "> 현재 구동 중인 웹 서버가 없습니다."
+else
+  echo "> 기존 서버 종료: kill -15 $CURRENT_PID"
+  kill -15 $CURRENT_PID
+  sleep 2
+fi
+
+echo "> 새로운 서버 시작"
+nohup python3 -m http.server 8080 --directory public > nohup.out 2>&1 &
